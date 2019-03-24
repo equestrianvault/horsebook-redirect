521e1d5a935119eb0a3f09c11ebf54ab9860c06b
Nathan Brown <nrb62281@gmail.com>
2019-03-23 23:55:24 -0700

    Updating redirects.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php // Permanent 301 Redirect via PHP
 	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: https://crystal-archives.netlify.com/");
+	header("Location: https://www.equestrianvault.com/");
 	exit();
 ?>
